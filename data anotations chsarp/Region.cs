7929b41df391166cdff080bc960b44256f095a2c
KEVIN SANDOVAL <kevinultra07@gmail.com>
2026-08-03 16:41:35 -0500

    Implement EntityConfiguration for Region entity
    Added EntityConfiguration method for Region entity.

--- data anotations chsarp/Region.cs
+++ data anotations chsarp/Region.cs
@@ -19,3 +19,27 @@ namespace tablas_ecoscolombia
         public List<Tadicion> Tradiciones { get; set; }
     }
 }
+public void EntityConfiguration(ModelBuilder modelBuilder)
+{
+    modelBuilder.Entity<Region>().ToTable("Region");
+
+    modelBuilder.Entity<Region>()
+        .HasKey(r => r.idRegion);
+
+    modelBuilder.Entity<Region>()
+        .Property(r => r.idRegion)
+        .HasColumnName("idRegion")
+        .ValueGeneratedOnAdd();
+
+    modelBuilder.Entity<Region>()
+        .Property(r => r.Nombre)
+        .HasColumnName("Nombre")
+        .HasMaxLength(100)
+        .IsRequired();
+
+    modelBuilder.Entity<Region>()
+        .HasMany(r => r.Tradiciones)
+        .WithOne(t => t.Region)
+        .HasForeignKey(t => t.idRegion)
+        .OnDelete(DeleteBehavior.Restrict);
+}
