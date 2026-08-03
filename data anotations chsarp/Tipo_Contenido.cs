1efef19bc560547b746b6f3d6c7cc1cfba504bbb
KEVIN SANDOVAL <kevinultra07@gmail.com>
2026-08-03 16:43:55 -0500

    Implement EntityConfiguration for Tipo_Contenido
    Added EntityConfiguration method for Tipo_Contenido.

--- data anotations chsarp/Tipo_Contenido.cs
+++ data anotations chsarp/Tipo_Contenido.cs
@@ -14,3 +14,26 @@ namespace tablas_ecoscolombia
 
     }
 }
+public void EntityConfiguration(ModelBuilder modelBuilder)
+{
+    modelBuilder.Entity<Tipo_Contenido>().ToTable("Tipo_Contenido");
+
+    modelBuilder.Entity<Tipo_Contenido>()
+        .HasKey(t => t.idContenido);
+
+    modelBuilder.Entity<Tipo_Contenido>()
+        .Property(t => t.idContenido)
+        .HasColumnName("idContenido")
+        .ValueGeneratedOnAdd();
+
+    modelBuilder.Entity<Tipo_Contenido>()
+        .Property(t => t.Nombre)
+        .HasColumnName("Nombre")
+        .HasMaxLength(100)
+        .IsRequired();
+
+    modelBuilder.Entity<Tipo_Contenido>()
+        .Property(t => t.Descipcion)
+        .HasColumnName("Descripcion")
+        .HasMaxLength(250);
+}
