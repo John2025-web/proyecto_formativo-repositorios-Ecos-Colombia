e3010951b489255e974eba73898df61681ddfa99
KEVIN SANDOVAL <kevinultra07@gmail.com>
2026-08-03 16:42:55 -0500

    Implement EntityConfiguration for Resena entity
    Added EntityConfiguration method to configure Resena entity properties and relationships.

--- data anotations chsarp/Resena.cs
+++ data anotations chsarp/Resena.cs
@@ -30,3 +30,46 @@ namespace tablas_ecoscolombia
         public LugarTuristico Lugar { get; set; }
     }
 }
+    public void EntityConfiguration(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Resena>().ToTable("Resena");
+
+        modelBuilder.Entity<Resena>().HasKey(r => r.idResena);
+
+        modelBuilder.Entity<Resena>()
+            .Property(r => r.idResena)
+            .HasColumnName("idResena")
+            .ValueGeneratedOnAdd();
+
+        modelBuilder.Entity<Resena>()
+            .Property(r => r.Comentario)
+            .HasColumnName("Comentario")
+            .HasMaxLength(500);
+
+        modelBuilder.Entity<Resena>()
+            .Property(r => r.Calificacion)
+            .HasColumnName("Calificacion")
+            .IsRequired();
+
+        modelBuilder.Entity<Resena>()
+            .Property(r => r.Fecha)
+            .HasColumnName("Fecha");
+
+        modelBuilder.Entity<Resena>()
+            .HasOne(r => r.Usuario)
+            .WithMany()
+            .HasForeignKey(r => r.idUsuario)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Resena>()
+            .HasOne(r => r.Lugar)
+            .WithMany()
+            .HasForeignKey(r => r.idLugar)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+
+    public async Task<bool> SaveAsync()
+    {
+        return await SaveChangesAsync() > 0;
+    }
+}
