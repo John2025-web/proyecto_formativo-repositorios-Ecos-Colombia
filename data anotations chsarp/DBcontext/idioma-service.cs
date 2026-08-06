539ddbaf7308a4942568d6118cfbffd493a6144a
kaleth <kalethstevendurancuenca@gmail.com>
2026-08-06 08:43:37 -0500

    context añadido a las clasescontext añadido al repositorioImplement EntityConfiguration for Tipo_Contenido
    Added EntityConfiguration method for Tipo_Contenido.

---
+++ data anotations chsarp/DBcontext/idioma-service.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pruebaApi.models;
+
+namespace pruebaApi.DBcontext
+{
+    public class idioma_service
+    {
+        public class DatabaseService : DbContext
+        {
+            public DatabaseService(DbContextOptions options) : base(options)
+            {
+            }
+            public DbSet<IDIOMA> IDIOMA { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                EntityConfiguration(modelBuilder);
+            }
+
+            private void EntityConfiguration(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<IDIOMA>().ToTable("IDIOMA   ");
+                modelBuilder.Entity<IDIOMA>().HasKey(u => u.CodigoIso);
+                modelBuilder.Entity<IDIOMA>().Property(u => u.IdiomaNombre).HasColumnName("id_culturaLugar").ValueGeneratedOnAdd();
+                
+
+            }
+            public async Task<bool> SaveAsync()
+            {
+                return await base.SaveChangesAsync() > 0;
+            }
+
+        }
+    }
+}
+
---
+++ data anotations chsarp/DBcontext/lugarTuristico-service.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pruebaApi.models;
+
+namespace pruebaApi.DBcontext
+{
+    public class LugarTuristicoservice
+    {
+        public class DatabaseService : DbContext
+        {
+            public DatabaseService(DbContextOptions options) : base(options)
+            {
+            }
+
+            public DbSet<LugarTuristico> LugarTuristico { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                EntityConfiguration(modelBuilder);
+            }
+
+            private void EntityConfiguration(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<LugarTuristico>().ToTable("lugar turistico");
+                modelBuilder.Entity<LugarTuristico>().HasKey(u => u.IdLugar);
+                modelBuilder.Entity<LugarTuristico>().Property(u => u.Nombre).HasColumnName("Nombre").ValueGeneratedOnAdd();
+                modelBuilder.Entity<LugarTuristico>().Property(u => u.Ciudad).HasColumnName("Ciudad").ValueGeneratedOnAdd();
+                modelBuilder.Entity<LugarTuristico>().Property(u => u.Descripcion).HasColumnName("descripcion").ValueGeneratedOnAdd();
+                modelBuilder.Entity<LugarTuristico>().Property(u => u.Imagen).HasColumnName("Nombre").ValueGeneratedOnAdd();
+                modelBuilder.Entity<LugarTuristico>().Property(u => u.ubicacion).HasColumnName("ubicacion").ValueGeneratedOnAdd();
+
+            }
+            public async Task<bool> SaveAsync()
+            {
+                return await base.SaveChangesAsync() > 0;
+            }
+
+        }
+    }
+}
