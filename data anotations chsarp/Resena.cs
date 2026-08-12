c3eb640de7323a6e913222d77701737584bc1591
KEVIN SANDOVAL <kevinultra07@gmail.com>
2026-08-12 14:24:21 -0500

    Refactor Resena class and add configuration static class

--- data anotations chsarp/Resena.cs
+++ data anotations chsarp/Resena.cs
@@ -1,26 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
-using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace tablas_ecoscolombia
 {
     public class Resena
     {
         [Key]
-        public int idResena {  get; set; }
-        
+        public int idResena { get; set; }
+
         public int idUsuario { get; set; }
         public int idLugar { get; set; }
-        
+
         public string Comentario { get; set; }
 
-        [System.ComponentModel.DataAnnotations.Range(1,5)]
+        [Range(1, 5)]
         public int Calificacion { get; set; }
+
         public DateTime? Fecha { get; set; }
 
         [ForeignKey("idUsuario")]
@@ -29,45 +30,49 @@ namespace tablas_ecoscolombia
         [ForeignKey("idLugar")]
         public LugarTuristico Lugar { get; set; }
     }
-}
-    public void EntityConfiguration(ModelBuilder modelBuilder)
+
+    public static class ResenaConfiguration
     {
-        modelBuilder.Entity<Resena>().ToTable("Resena");
+        public static void EntityConfiguration(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Resena>().ToTable("Resena");
+            modelBuilder.Entity<Resena>().HasKey(r => r.idResena);
 
-        modelBuilder.Entity<Resena>().HasKey(r => r.idResena);
+            modelBuilder.Entity<Resena>()
+                .Property(r => r.idResena)
+                .HasColumnName("idResena")
+                .ValueGeneratedOnAdd();
 
-        modelBuilder.Entity<Resena>()
-            .Property(r => r.idResena)
-            .HasColumnName("idResena")
-            .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Resena>()
+                .Property(r => r.Comentario)
+                .HasColumnName("Comentario")
+                .HasMaxLength(500);
 
-        modelBuilder.Entity<Resena>()
-            .Property(r => r.Comentario)
-            .HasColumnName("Comentario")
-            .HasMaxLength(500);
+            modelBuilder.Entity<Resena>()
+                .Property(r => r.Calificacion)
+                .HasColumnName("Calificacion")
+                .IsRequired();
 
-        modelBuilder.Entity<Resena>()
-            .Property(r => r.Calificacion)
-            .HasColumnName("Calificacion")
-            .IsRequired();
+            modelBuilder.Entity<Resena>()
+                .Property(r => r.Fecha)
+                .HasColumnName("Fecha");
 
-        modelBuilder.Entity<Resena>()
-            .Property(r => r.Fecha)
-            .HasColumnName("Fecha");
+            modelBuilder.Entity<Resena>()
+                .HasOne(r => r.Usuario)
+                .WithMany()
+                .HasForeignKey(r => r.idUsuario)
+                .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<Resena>()
-            .HasOne(r => r.Usuario)
-            .WithMany()
-            .HasForeignKey(r => r.idUsuario)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<Resena>()
-            .HasOne(r => r.Lugar)
-            .WithMany()
-            .HasForeignKey(r => r.idLugar)
-            .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Resena>()
+                .HasOne(r => r.Lugar)
+                .WithMany()
+                .HasForeignKey(r => r.idLugar)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
-
+}
+public class EcosColombiaContext : DbContext
+{
     public async Task<bool> SaveAsync()
     {
         return await SaveChangesAsync() > 0;
