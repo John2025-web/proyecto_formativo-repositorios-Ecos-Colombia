1f146260a30c4a6d0577fe912342c0a402b88bc2
KEVIN SANDOVAL <kevinultra07@gmail.com>
2026-08-12 14:23:57 -0500

    Refactor Tipo_Contenido class and configuration

--- data anotations chsarp/Tipo_Contenido.cs
+++ data anotations chsarp/Tipo_Contenido.cs
@@ -1,39 +1,48 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace tablas_ecoscolombia
 {
     public class Tipo_Contenido
     {
-        public int idContenido {  get; set; }
+        [Key]
+        public int idContenido { get; set; }
+
         public string Nombre { get; set; }
-        public string Descipcion { get; set; }
+
+        public string Descipcion { get; set; } 
 
     }
-}
-public void EntityConfiguration(ModelBuilder modelBuilder)
-{
-    modelBuilder.Entity<Tipo_Contenido>().ToTable("Tipo_Contenido");
 
-    modelBuilder.Entity<Tipo_Contenido>()
-        .HasKey(t => t.idContenido);
+    public static class TipoContenidoConfiguration
+    {
+        public static void EntityConfiguration(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tipo_Contenido>().ToTable("Tipo_Contenido");
 
-    modelBuilder.Entity<Tipo_Contenido>()
-        .Property(t => t.idContenido)
-        .HasColumnName("idContenido")
-        .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Tipo_Contenido>()
+                .HasKey(t => t.idContenido);
 
-    modelBuilder.Entity<Tipo_Contenido>()
-        .Property(t => t.Nombre)
-        .HasColumnName("Nombre")
-        .HasMaxLength(100)
-        .IsRequired();
+            modelBuilder.Entity<Tipo_Contenido>()
+                .Property(t => t.idContenido)
+                .HasColumnName("idContenido")
+                .ValueGeneratedOnAdd();
 
-    modelBuilder.Entity<Tipo_Contenido>()
-        .Property(t => t.Descipcion)
-        .HasColumnName("Descripcion")
-        .HasMaxLength(250);
+            modelBuilder.Entity<Tipo_Contenido>()
+                .Property(t => t.Nombre)
+                .HasColumnName("Nombre")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<Tipo_Contenido>()
+                .Property(t => t.Descipcion)
+                .HasColumnName("Descripcion")
+                .HasMaxLength(250);
+        }
+    }
 }
