bdddab369a5c4c1791ccaff9ad8d8b8e607fdde2
KEVIN SANDOVAL <kevinultra07@gmail.com>
2026-08-12 14:23:25 -0500

    Refactor Tradicion class and add configuration

--- data anotations chsarp/Tradicion.cs
+++ data anotations chsarp/Tradicion.cs
@@ -1,51 +1,59 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace tablas_ecoscolombia
 {
     public class Tradicion
     {
+        [Key]
         public int idTradicion { get; set; }
+
         public string Nombre { get; set; }
         public int idRegion { get; set; }
         public DateTime? FechaCelebracion { get; set; }
-        
+
         public Region Region { get; set; }
     }
-}
-public void EntityConfiguration(ModelBuilder modelBuilder)
-{
-    modelBuilder.Entity<Tradicion>().ToTable("Tradicion");
 
-    modelBuilder.Entity<Tradicion>()
-        .HasKey(t => t.idTradicion);
+    public static class TradicionConfiguration
+    {
+        public static void EntityConfiguration(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tradicion>().ToTable("Tradicion");
 
-    modelBuilder.Entity<Tradicion>()
-        .Property(t => t.idTradicion)
-        .HasColumnName("idTradicion")
-        .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Tradicion>()
+                .HasKey(t => t.idTradicion);
 
-    modelBuilder.Entity<Tradicion>()
-        .Property(t => t.Nombre)
-        .HasColumnName("Nombre")
-        .HasMaxLength(150)
-        .IsRequired();
+            modelBuilder.Entity<Tradicion>()
+                .Property(t => t.idTradicion)
+                .HasColumnName("idTradicion")
+                .ValueGeneratedOnAdd();
 
-    modelBuilder.Entity<Tradicion>()
-        .Property(t => t.idRegion)
-        .HasColumnName("idRegion")
-        .IsRequired();
+            modelBuilder.Entity<Tradicion>()
+                .Property(t => t.Nombre)
+                .HasColumnName("Nombre")
+                .HasMaxLength(150)
+                .IsRequired();
 
-    modelBuilder.Entity<Tradicion>()
-        .Property(t => t.FechaCelebracion)
-        .HasColumnName("FechaCelebracion");
+            modelBuilder.Entity<Tradicion>()
+                .Property(t => t.idRegion)
+                .HasColumnName("idRegion")
+                .IsRequired();
 
-    modelBuilder.Entity<Tradicion>()
-        .HasOne(t => t.Region)
-        .WithMany()
-        .HasForeignKey(t => t.idRegion)
-        .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Tradicion>()
+                .Property(t => t.FechaCelebracion)
+                .HasColumnName("FechaCelebracion");
+
+            modelBuilder.Entity<Tradicion>()
+                .HasOne(t => t.Region)
+                .WithMany(r => r.Tradiciones)
+                .HasForeignKey(t => t.idRegion)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
 }
