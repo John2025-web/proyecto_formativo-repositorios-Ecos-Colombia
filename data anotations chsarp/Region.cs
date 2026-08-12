976970e1a4868ee480dc46ff621acc6055f62b89
KEVIN SANDOVAL <kevinultra07@gmail.com>
2026-08-12 14:24:45 -0500

    Update Region.cs

--- data anotations chsarp/Region.cs
+++ data anotations chsarp/Region.cs
@@ -1,45 +1,50 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace tablas_ecoscolombia
 {
     public class Region
     {
         [Key]
-        public int idRegion {  get; set; }
-        
+        public int idRegion { get; set; }
+
         [Required]
         [MaxLength(100)]
         public string Nombre { get; set; }
 
-        public List<Tadicion> Tradiciones { get; set; }
+        public List<Tradicion> Tradiciones { get; set; }
     }
-}
-public void EntityConfiguration(ModelBuilder modelBuilder)
-{
-    modelBuilder.Entity<Region>().ToTable("Region");
 
-    modelBuilder.Entity<Region>()
-        .HasKey(r => r.idRegion);
+    public static class RegionConfiguration
+    {
+        public static void EntityConfiguration(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Region>().ToTable("Region");
 
-    modelBuilder.Entity<Region>()
-        .Property(r => r.idRegion)
-        .HasColumnName("idRegion")
-        .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Region>()
+                .HasKey(r => r.idRegion);
 
-    modelBuilder.Entity<Region>()
-        .Property(r => r.Nombre)
-        .HasColumnName("Nombre")
-        .HasMaxLength(100)
-        .IsRequired();
+            modelBuilder.Entity<Region>()
+                .Property(r => r.idRegion)
+                .HasColumnName("idRegion")
+                .ValueGeneratedOnAdd();
 
-    modelBuilder.Entity<Region>()
-        .HasMany(r => r.Tradiciones)
-        .WithOne(t => t.Region)
-        .HasForeignKey(t => t.idRegion)
-        .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Region>()
+                .Property(r => r.Nombre)
+                .HasColumnName("Nombre")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            modelBuilder.Entity<Region>()
+                .HasMany(r => r.Tradiciones)
+                .WithOne(t => t.Region)
+                .HasForeignKey(t => t.idRegion)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
 }
